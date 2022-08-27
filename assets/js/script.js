086e930c8d437a8ea1a4f2262a840938535a592b
lucasgrav <correalucas1905@gmail.com>
2022-08-27 11:08:16 -0300

    arregleToLowe

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,19 @@ else if(vengadorIngresado.toLowerCase() == "hulk"){
     alert("HULK ENOJADO APLASTAAAAAAAA!")
     condicion = false
 }
-else if (vengadorIngresado.toLowerCase == "thor"){
+else if (vengadorIngresado.toLowerCase() == "thor"){
     alert("Yo soy Thor, hijo de Odín y mientras haya vida en mi pecho… No se me ocurre más nada que decir. ¿Estás listo para morir?")
     condicion = false
 }
-else if (vengadorIngresado.toLowerCase == "hawkeye"){
+else if (vengadorIngresado.toLowerCase() == "hawkeye"){
     alert("Tú y yo recordamos Budapest de forma muy diferente")
     condicion = false
 }
-else if (vengadorIngresado.toLowerCase == "la viuda negra"){
+else if (vengadorIngresado.toLowerCase() == "la viuda negra"){
     alert("Antes no tenía nada y ahora tengo esto. Este trabajo, esta familia, y eso me hizo mejor de lo que era, y aunque todos se han ido, todavía trato de ser mejor.")
     condicion = false
 }
-else if (vengadorIngresado.toLowerCase == "capitan america"){
+else if (vengadorIngresado.toLowerCase() == "capitan america"){
     alert ("Puedo hacer esto todo el dia")
     condicion = false
 }
