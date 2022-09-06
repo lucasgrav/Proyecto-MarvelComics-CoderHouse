e161773d4b27679980c5f3bf1413e1f2666a4770
lucasgrav <correalucas1905@gmail.com>
2022-09-06 16:39:42 -0300

    casiListaPreentrega

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@
 let condicion = true;
 //ARRAYS
 const comics = [
-  {id:1, nombre: "Iron Man", precio: 1500, editorial: "Marvel"},
-  {id:2,nombre: "Capitan America", precio: 1500, editorial: "Marvel"},
-  {id:3,nombre: "SpiderMan", precio: 2000, editorial: "Marvel"},
-  {id:4,nombre: "Batman", precio: 2200, editorial: "Dc"},
-  {id:5,nombre: "Los jovenes titanes", precio: 1700, editorial: "Dc"},
-  {id:6,nombre: "El acertijo", precio: 2500, editorial: "Dc"}
-]
+  { id: 1, nombre: "Iron Man", precio: 1500, editorial: "Marvel" },
+  { id: 2, nombre: "Capitan America", precio: 1500, editorial: "Marvel" },
+  { id: 3, nombre: "SpiderMan", precio: 2000, editorial: "Marvel" },
+  { id: 4, nombre: "Batman", precio: 2200, editorial: "Dc" },
+  { id: 5, nombre: "Los jovenes titanes", precio: 1700, editorial: "Dc" },
+  { id: 6, nombre: "El acertijo", precio: 2500, editorial: "Dc" },
+];
 //FUNCIONES
 function cuotasFuncion(nombreComic, cantidadCuotas, precioComic) {
   let divisionCuotas = parseInt(precioComic / cantidadCuotas);
@@ -32,16 +32,21 @@ function cuotasFuncion(nombreComic, cantidadCuotas, precioComic) {
   }
 }
 //FUNCION PARA OPCION 1
-function compraDeComics(){
+function compraDeComics() {
   let comprarComics = parseInt(
     prompt(
       "Que comic desea comprar?:\n(ACLARACION: coloque solamente el numero del comic!\n1) IronMan\n2) SpiderMan\n3) Capitan America\n4) Batman\n5) Los jovenes titanes\n6) El acertijo"
     )
   );
-  if (comprarComics == 1 || comprarComics == 2 || comprarComics == 3 || comprarComics == 4 || comprarComics == 5 || comprarComics == 6) {
-   
-    let comicSeleccionado = comics.find(comic => comic.id == comprarComics)
-    
+  if (
+    comprarComics == 1 ||
+    comprarComics == 2 ||
+    comprarComics == 3 ||
+    comprarComics == 4 ||
+    comprarComics == 5 ||
+    comprarComics == 6
+  ) {
+    let comicSeleccionado = comics.find((comic) => comic.id == comprarComics);
 
     let comprarComic = prompt(
       `El precio del comic de ${comicSeleccionado.nombre} es de $${comicSeleccionado.precio}, desea comprarlo? coloque si o no`
@@ -51,7 +56,11 @@ function compraDeComics(){
         let cuotas = parseInt(
           prompt("Desea pagarlo en 1, 2 o 3 cuotas sin interes?")
         );
-        cuotasFuncion(comicSeleccionado.nombre, cuotas, comicSeleccionado.precio);
+        cuotasFuncion(
+          comicSeleccionado.nombre,
+          cuotas,
+          comicSeleccionado.precio
+        );
         break;
 
       case "no":
@@ -62,48 +71,53 @@ function compraDeComics(){
         alert("Opcion incorrecta, volvamos a empezar!");
         break;
     }
+  } else {
+    alert("Ese digito no corresponde a ningun comic, intentalo de vuelta!");
   }
-  else{
-    alert("Ese digito no corresponde a ningun comic, intentalo de vuelta!")
-  }
-
 }
 //FUNCION PARA OPCION 2
-function busquedaDeComic(){
-  let editorialDeComic = prompt("Que comics desea ver: Marvel o Dc?")
-  let editorialElegida = comics.filter(cEditorial => cEditorial.editorial.toLowerCase == editorialDeComic.toLowerCase)
-  console.log(editorialElegida)
-
-
+function busquedaDeComic() {
+  let busquedaComic = prompt("Que comic desea ver?").toLowerCase();
+  let resultadoBusqueda = comics.filter(
+    (comic) =>
+      comic.editorial.toLowerCase().indexOf(busquedaComic) > -1 ||
+      comic.nombre.toLowerCase().indexOf(busquedaComic) > -1
+  );
 }
 
 // FUNCION PARA OPCION 3
-function listadoDeComics(){
- let listaComics = ""
-  for(const comic of comics){
-    listaComics = listaComics + (`Comic: ${comic.nombre} / Editorial: ${comic.editorial}\n`)
+function listadoDeComics(lista) {
+  let listaComics = "";
+  for (const comic of lista) {
+    listaComics =
+      listaComics + `Comic: ${comic.nombre} / Editorial: ${comic.editorial}\n`;
   }
-  alert(`Puede ver el listado de comics: \n ${listaComics}`)
+  alert(`Puede ver el listado de comics: \n ${listaComics}`);
 }
-                                                                                                    
+
 //CICLO
 do {
-  let menuDePagina = parseInt (prompt("Bienvenido a ComicZone que desea realizar:  \n1- Comprar comics  \n2- Buscar comics por editorial  \n3- Ver Listado de Comics \n4- Salir")
-  )
-  switch(menuDePagina){
+  let menuDePagina = parseInt(
+    prompt(
+      "Bienvenido a ComicZone que desea realizar:  \n1- Comprar comics  \n2- Buscar comics por editorial  \n3- Ver Listado de Comics \n4- Salir"
+    )
+  );
+  switch (menuDePagina) {
     case 1:
-    compraDeComics()
-    break;
+      compraDeComics();
+      break;
     case 2:
-    busquedaDeComic()
-    break;
+      busquedaDeComic();
+      break;
     case 3:
-   listadoDeComics()
-    break;
+      listadoDeComics(comics);
+      break;
     case 4:
-      alert("Muchas gracias por visitarnos! Vuelva Pronto!")
-      condicion = false
-    break;
-  
+      alert("Muchas gracias por visitarnos! Vuelva Pronto!");
+      condicion = false;
+      break;
+    default:
+      alert("Digito incorrecto, intentelo de nuevo!");
+      break;
   }
 } while (condicion);
